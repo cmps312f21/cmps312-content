c5bf0b0161f7b141d2e48f9f3711654ba18a2465
ae <erradi@live.com>
2021-09-11 01:55:56 +0300

    Updated Compose examples

--- examples/04.compose-ui/old-compose-ui/app/src/main/java/qa/edu/cmps312/compose/NavigationItem.kt
+++
@@ -1,5 +0,0 @@
-package qa.edu.cmps312.compose
-
-sealed class NavigationItem(var route: String, var icon: Int, var title: String) {
-    object Home : NavigationItem("home", R.drawable.ic_home, "Home")
-}
--- examples/04.compose-ui/old-compose-ui/app/src/main/java/qa/edu/cmps312/compose/Utils.kt
+++
@@ -1,8 +0,0 @@
-package qa.edu.cmps312.compose
-
-import android.graphics.Color
-
-fun getRandomColor() : Int {
-    val rgbValues = (0..255).shuffled().take(4)
-    return Color.argb(rgbValues[0], rgbValues[1], rgbValues[2], rgbValues[3])
-}
