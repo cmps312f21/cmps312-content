ab0d10127426c74b0ebe7e94d99b81dac040e479
ae <erradi@live.com>
2021-09-25 02:23:33 +0300

    Update ScoreScreen.kt

--- examples/8.1.Football-MVVM/app/src/main/java/cmps312/football/view/ScoreScreen.kt
+++ examples/8.1.Football-MVVM/app/src/main/java/cmps312/football/view/ScoreScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cmps312.football.R
 import cmps312.football.ui.theme.FootballMVVMTheme
@@ -34,44 +35,53 @@ fun ScoreScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-    Row(
-        modifier = Modifier.weight(3F).fillMaxSize(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        TeamScore(
-            modifier = Modifier.weight(1F),
-            iconId = R.drawable.img_rayyan,
-            score = scoreViewModel.team1Score.value
-        ) { //team1Score) {
-            scoreViewModel.onIncrementTeam1Score()
-            //team1Score++
+        Row(
+            modifier = Modifier
+                .weight(3F)
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            TeamScore(
+                modifier = Modifier.weight(1F),
+                iconId = R.drawable.img_rayyan,
+                score = scoreViewModel.team1Score.value
+            ) { //team1Score) {
+                scoreViewModel.onIncrementTeam1Score()
+                //team1Score++
+            }
+            TeamScore(
+                modifier = Modifier.weight(1F),
+                iconId = R.drawable.img_alkhor,
+                score = scoreViewModel.team2Score.value
+            ) { //team2Score) {
+                scoreViewModel.onIncrementTeam2Score()
+                //team2Score++
+            }
         }
-        TeamScore(
-            modifier = Modifier.weight(1F),
-            iconId = R.drawable.img_alkhor,
-            score = scoreViewModel.team2Score.value
-        ) { //team2Score) {
-            scoreViewModel.onIncrementTeam2Score()
-            //team2Score++
+        Box(
+            Modifier.weight(1F),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                // Recomposes whenever redCardsCount changes
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = "Red cards count: ${redCardsCount.value}"
+                )
+                // Recomposes whenever timeRemaining changes
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = timeRemaining.value
+                )
+                // Recomposes whenever newsUpdate changes
+                Text(
+                    modifier = Modifier.padding(bottom = 16.dp),
+                    textAlign = TextAlign.Center,
+                    text = "\uD83D\uDCE2 ${newsUpdate.value}"
+                )
+            }
         }
-    }
-        // Recomposes whenever redCardsCount changes
-        Text(
-            modifier = Modifier.weight(0.5F),
-            textAlign = TextAlign.Center,
-            text = "Red cards count: ${redCardsCount.value}"
-        )
-        Text(
-            modifier = Modifier.weight(0.5F),
-            textAlign = TextAlign.Center,
-            text = timeRemaining.value
-        )
-        Text(
-            modifier = Modifier.weight(0.5F),
-            textAlign = TextAlign.Center,
-            text = "\uD83D\uDCE2 ${newsUpdate.value}"
-        )
     }
 }
 
