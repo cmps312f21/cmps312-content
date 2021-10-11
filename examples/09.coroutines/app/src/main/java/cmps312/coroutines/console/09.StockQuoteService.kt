111d1347afb4d63441471febeeccf1ef318e17e3
ae <erradi@live.com>
2021-10-11 22:59:31 +0300

    Updated coroutine examples

--- examples/09.coroutines/app/src/main/java/cmps312/coroutines/console/09.StockQuoteService.kt
+++ examples/09.coroutines/app/src/main/java/cmps312/coroutines/console/09.StockQuoteService.kt
@@ -1,22 +1,19 @@
 package cmps312.coroutines.console
 
-import cmps312.coroutines.webapi.getStockQuote
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import cmps312.coroutines.viewmodel.MainViewModel
+import cmps312.coroutines.webapi.StockQuoteService
+import kotlinx.coroutines.*
 
-fun main() = runBlocking<Unit> {
+suspend fun main() {
     val startTime = System.currentTimeMillis()
-    val job = GlobalScope.launch {
+    val job = CoroutineScope(Dispatchers.IO).launch {
         val company = "Apple"
         val date = "2021-10-08"
 
         val viewModel = MainViewModel()
         val symbol = viewModel.getStockSymbol(company)
 
-        // ToDo: call the GetQuote service
-        val quote = getStockQuote(symbol, date)
+        val quote = StockQuoteService.getStockQuote(symbol, date)
         println(">> $company (${quote.symbol}) = ${quote.price}")
         println(">> MarketStockQuote: $quote")
     }
