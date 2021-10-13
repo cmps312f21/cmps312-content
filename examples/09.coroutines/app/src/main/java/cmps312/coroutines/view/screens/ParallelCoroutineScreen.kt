12579f6d3b7e074fabf4c23d15838a29c5711807
ae <erradi@live.com>
2021-10-13 22:14:17 +0300

    Updated coroutine examples

--- examples/09.coroutines/app/src/main/java/cmps312/coroutines/view/screens/ParallelCoroutineScreen.kt
+++ examples/09.coroutines/app/src/main/java/cmps312/coroutines/view/screens/ParallelCoroutineScreen.kt
@@ -10,19 +10,23 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cmps312.coroutines.model.StockQuote
 import cmps312.coroutines.view.components.ClickCounter
 import cmps312.coroutines.view.components.Dropdown
 import cmps312.coroutines.view.components.TopBar
+import cmps312.coroutines.view.displayMessage
 import cmps312.coroutines.view.removeTrailingComma
 import cmps312.coroutines.viewmodel.StockQuoteViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
-enum class UiState {
-    LOADING,
+enum class JobState {
+    RUNNING,
     SUCCESS
     //ERROR
 }
@@ -30,18 +34,19 @@ enum class UiState {
 @Composable
 fun ParallelCoroutineScreen() {
     val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
     val viewModel = viewModel<StockQuoteViewModel>()
 
     var companyList = remember { mutableStateListOf<String>() }
 
     var selectedCompany by remember { mutableStateOf("") }
-    var getStockQuoteState by remember { mutableStateOf(UiState.SUCCESS) }
+    var getStockQuoteState by remember { mutableStateOf(JobState.SUCCESS) }
     var stockQuote by remember { mutableStateOf(StockQuote()) }
 
     var companies by remember { mutableStateOf("Tesla, Apple, Microsoft, IBM,") }
     var companyStockQuotes = remember { mutableStateListOf<StockQuote>() }
     var runInParallel by remember { mutableStateOf(true) }
-    var getStockQuotesState by remember { mutableStateOf(UiState.SUCCESS) }
+    var getStockQuotesState by remember { mutableStateOf(JobState.SUCCESS) }
     var executionDuration by remember { mutableStateOf(0L) }
 
     // LaunchedEffect will be executed when the composable is first launched
@@ -50,20 +55,39 @@ fun ParallelCoroutineScreen() {
         companyList.addAll(viewModel.getCompanies())
     }
 
+    val scaffoldState = rememberScaffoldState()
+
+    val exceptionHandler = CoroutineExceptionHandler { coroutineContext, exception ->
+        val msg = exception.message ?: "Request failed"
+        displayMessage(LocalContext.current, msg)
+        println("Debug: $msg")
+        //Toast.makeText(requireContext(), msg, Toast.LENGTH_LONG).show()
+        // Show a snackbar using to dislay a message
+        /*coroutineScope.launch {
+            scaffoldState.snackbarHostState.showSnackbar(
+                message = msg
+                //actionLabel = "Retry message"
+            )
+        }*/
+    }
+
     fun onGetStockQuotesClick() {
         val startTime = System.currentTimeMillis()
-        getStockQuotesState = UiState.LOADING
+        getStockQuotesState = JobState.RUNNING
         executionDuration = 0L
         companyStockQuotes.clear()
         val companiesList = companies.trim().removeTrailingComma().split(",")
-        val job = coroutineScope.launch {
 
-            val stockQuotesFlow = if (runInParallel)
-                viewModel.getStockQuotesInParallel(companiesList)
-            else
-                viewModel.getStockQuotes(companiesList)
+        val stockQuotesFlow = if (runInParallel)
+            viewModel.getStockQuotesInParallel(companiesList)
+        else
+            viewModel.getStockQuotes(companiesList)
 
-            stockQuotesFlow.collect {
+        val job = coroutineScope.launch(exceptionHandler) {
+            stockQuotesFlow.catch { e ->
+                val msg = e.message ?: "Request failed"
+                displayMessage(context, msg)
+            }.collect {
                 println(it)
                 companyStockQuotes.add(it)
             }
@@ -71,14 +95,16 @@ fun ParallelCoroutineScreen() {
 
         job.invokeOnCompletion {
             executionDuration = System.currentTimeMillis() - startTime
-            getStockQuotesState = UiState.SUCCESS
+            getStockQuotesState = JobState.SUCCESS
             println(">>> Debug: Job done. Total execution time: ${executionDuration / 1000}s")
         }
     }
 
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = { TopBar("Sequential vs. Parallel Coroutines") }
     ) {
+
         Column(
             Modifier.padding(8.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -90,18 +116,18 @@ fun ParallelCoroutineScreen() {
                 onSelectionChange = {
                     selectedCompany = it
                     coroutineScope.launch {
-                        getStockQuoteState = UiState.LOADING
+                        getStockQuoteState = JobState.RUNNING
                         stockQuote = viewModel.getStockQuote(selectedCompany)
-                        getStockQuoteState = UiState.SUCCESS
+                        getStockQuoteState = JobState.SUCCESS
                     }
                 }
             )
 
             when (getStockQuoteState) {
-                UiState.LOADING -> {
+                JobState.RUNNING -> {
                     CircularProgressIndicator()
                 }
-                UiState.SUCCESS -> {
+                JobState.SUCCESS -> {
                     Text(text = "$stockQuote")
                 }
             }
@@ -138,7 +164,7 @@ fun ParallelCoroutineScreen() {
                 }
             }
 
-            if (getStockQuotesState == UiState.LOADING)
+            if (getStockQuotesState == JobState.RUNNING)
                 CircularProgressIndicator()
 
             LazyColumn(
@@ -170,4 +196,3 @@ fun ParallelCoroutineScreen() {
         }
     }
 }
-
--- examples/09.coroutines/app/src/main/java/cmps312/coroutines/viewmodel/StockQuoteViewModel.kt
+++ examples/09.coroutines/app/src/main/java/cmps312/coroutines/viewmodel/StockQuoteViewModel.kt
@@ -1,18 +1,22 @@
 package cmps312.coroutines.viewmodel
 
+import android.util.Log
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import cmps312.coroutines.model.StockQuote
+import cmps312.coroutines.view.displayMessage
 import cmps312.coroutines.webapi.SimulatedStockQuoteService
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 private const val TAG = "ViewModel.Coroutines"
 class StockQuoteViewModel : ViewModel() {
     private val stockQuoteService = SimulatedStockQuoteService()
 
     suspend fun getStockQuote(name: String) =
-         stockQuoteService.getStockQuote(name)
+        stockQuoteService.getStockQuote(name)
 
     suspend fun getCompanies(): List<String> =
         stockQuoteService.getCompanies()
@@ -24,16 +28,21 @@ class StockQuoteViewModel : ViewModel() {
         }
     }
 
+    val exceptionHandler = CoroutineExceptionHandler { coroutineContext, exception ->
+        val msg = exception.message ?: "Request failed"
+        //displayMessage(LocalContext.current, msg)
+        println("Debug: $msg")
+    }
+
     fun getStockQuotesInParallel(companies: List<String>) = flow {
         val deferredStockQuotes = mutableListOf<Deferred<StockQuote>>()
 
         // Launch get stock quotes in parallel
-        viewModelScope.launch {
+        viewModelScope.launch(exceptionHandler) {
             companies.forEach {
                 deferredStockQuotes.add(async { stockQuoteService.getStockQuote(it) })
             }
         }
-
         // Await for and emit the result
         deferredStockQuotes.forEach {
             val quote = it.await()
