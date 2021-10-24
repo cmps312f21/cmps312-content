5e05d5c3fb0126babac31b3bd400cac4be0f9e0f
ae <erradi@live.com>
2021-10-24 12:17:05 +0300

    Create 17.WebAPI-Examples.kt

---
+++ examples/09.coroutines/app/src/main/java/cmps312/coroutines/console/17.WebAPI-Examples.kt
@@ -0,0 +1,36 @@
+package cmps312.coroutines.console
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import cmps312.coroutines.webapi.SimulatedStockQuoteService
+import cmps312.coroutines.webapi.StockQuoteService
+import cmps312.coroutines.webapi.ToDo
+import cmps312.coroutines.webapi.ToDoService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@RequiresApi(Build.VERSION_CODES.O)
+suspend fun main() {
+    val job = CoroutineScope(Dispatchers.IO).launch {
+        /*
+        val toDos = ToDoService.getToDos()
+        println(">> ToDos\n: $toDos")
+        */
+
+        val toDo1 = ToDoService.getToDo(1)
+        println(">> ToDos\n: $toDo1")
+
+        /*
+        var toDo = ToDo(title = "Study Coroutines")
+        toDo = ToDoService.addToDo(toDo)
+        println(">> New ToDo \n: $toDo")
+       */
+
+        val isOk = ToDoService.deleteToDo(1)
+        println(">> Was delete Ok \n: $isOk")
+    }
+
+    // Wait for the job to finish otherwise main will exit
+    job.join()
+}
